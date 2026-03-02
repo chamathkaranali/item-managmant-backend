a0e632ff964980d18f8c49b2d8a012a800979a45
Chamathka Ranali <ranalichamathka@gmail.com>
2026-03-02 10:59:06 +0530

    feat: enhance item management table with CRUD functionality and remove unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,5 @@
 import { Routes } from '@angular/router';
-import { Additem } from './page/additem/additem';
-import { GetItem } from './page/get-item/get-item';
 
 export const routes: Routes = [
-    {
-        path:'addItem',
-        component:Additem
-    },
-    {
-        path:'get',
-        component:GetItem
-    }
+
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,55 @@
 import { NgForOf } from '@angular/common';
-import { ChangeDetectorRef, Component, OnInit, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Additem } from "./page/additem/additem";
-import { GetItem } from "./page/get-item/get-item";
-import { initFlowbite } from 'flowbite';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
-   ngOnInit(): void {
-    initFlowbite();
-  }
-  protected readonly title = signal('item-managmant-backend');
-}
+export class App {
+  itemlist: any = null;
 
-// cdr: ChangeDetectorRef
-// this.cdr.detectChanges()
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {
+    this.localData();
+  }
+
+  localData() {
+    this.http.get("http://localhost:8080/get").subscribe(data => {
+      this.itemlist = data;
+      this.cdr.detectChanges();
+      console.log(data);
+    });
+  }
+
+  updateItem(item: any) {
+    this.http.put("http://localhost:8080/update", item).subscribe({
+      next: (response) => {
+        alert("Item updated successfully!");
+        this.localData();
+      },
+      error: (error) => {
+        console.error("Error updating item", error);
+        alert("Failed to update the item.");
+      }
+    });
+  }
+
+  deleteItem(itemCode: String) {
+    if (confirm("Are you sure you want to delete this item?")) {
+      
+        this.http.delete(`http://localhost:8080/delete/${itemCode}`).subscribe({
+        next: (response) => {
+          alert("Item deleted successfully!");
+          this.localData();
+        },
+        error: (error) => {
+          console.error("Error deleting item", error);
+          alert("Failed to delete the item.");
+        }
+      });
+    }
+  }
+}
